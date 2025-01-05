907fa0b478741e1bddc20bffad2dc56e527bbc5f
Lina Torovoltas <193523522+lina-torovoltas@users.noreply.github.com>
2025-01-05 06:05:15 +0000

    Update main.py
    Comments to the code have been added completely!

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,11 @@ import re
 from telebot.async_telebot import AsyncTeleBot
 from difflib import SequenceMatcher
 
+
+
 bot = AsyncTeleBot('TOKEN')
+
+
 
 # User verification function for administrator rights
 async def is_user_admin(chat_id, user_id):
@@ -16,7 +20,9 @@ async def is_user_admin(chat_id, user_id):
         return 1
     return 0
 
-# Adding a user to the user monitoring list
+
+
+# Adding a user to the users monitoring list
 @bot.message_handler(commands=['add_user'])
 async def add_user(message):
     chat_id = message.chat.id
@@ -56,6 +62,8 @@ async def add_user(message):
     except Exception as e:
         await bot.reply_to(message, f"An error occurred: {str(e)}")
 
+
+# Adding a swear word to the list of swear words
 @bot.message_handler(commands=['add_swear'])
 async def add_swear(message):
     chat_id = message.chat.id
@@ -85,7 +93,7 @@ async def add_swear(message):
             await bot.reply_to(message, f"The swear word '{swear_word_to_add}' already exists in the list.")
             return
 
-        # # Adding a swear word to the list
+        # Adding a swear word to the list
         with open('swearing.txt', 'a') as file:
             file.write(f'\n{swear_word_to_add}')
 
@@ -95,6 +103,8 @@ async def add_swear(message):
     except Exception as e:
         await bot.reply_to(message, f"An error occurred: {str(e)}")
 
+
+# Removing a user from the users monitoring list
 @bot.message_handler(commands=['remove_user'])
 async def remove_user(message):
     chat_id = message.chat.id
@@ -125,47 +135,58 @@ async def remove_user(message):
                 if user.strip() != user_id_to_remove:
                     file.write(user)
 
-        # Send a message that the user has been successfully removed from the list
+        # Sending a message that the user has been successfully removed from the list
         await bot.reply_to(message, f"User ID: {user_id_to_remove} has been removed.")
-    # Send an error message that the file with the list of users was not found
+    # Sending an error message that the file with the list of users was not found
     except FileNotFoundError:
         await bot.reply_to(message, "User list not found.")
     # Sending an error message
     except Exception as e:
         await bot.reply_to(message, f"An error occurred: {str(e)}")
 
+
+# Removing a swear word from the list of swear words
 @bot.message_handler(commands=['remove_swear'])
 async def remove_swear(message):
     chat_id = message.chat.id
     user_id = message.from_user.id
     result = await is_user_admin(chat_id, user_id)
+
+    # Checking a user for administrator rights
     if result == 0:
         await bot.reply_to(message, "You do not have permission to run this command.")
         return
-    
+    # Checking a command for an argument
     args = message.text.split()
     if len(args) < 2:
         await bot.reply_to(message, "Please enter a swear word. \nExample: /remove_swear FUCK")
         return
 
     swear_word_to_remove = args[1]
-    print(f"Removing a swear word: {swear_word_to_remove}")
+    print(f"Removing a swear word: {swear_word_to_remove}")  # Debugging to the console
 
+    # Handling errors and removing swear word from the list
     try:
         with open('swearing.txt', 'r') as file:
             swearing = file.readlines()
 
+        # Removing a swear word from the list
         with open('swearing.txt', 'w') as file:
             for swear in swearing:
                 if swear.strip() != swear_word_to_remove:
                     file.write(swear)
 
+        # Sending a message that the swear word has been successfully removed from the list
         await bot.reply_to(message, f"Swear word '{swear_word_to_remove}' removed.")
+    # Sending an error message that the swear words list file was not found
     except FileNotFoundError:
         await bot.reply_to(message, "Swearing list not found.")
+    # Sending an error message
     except Exception as e:
         await bot.reply_to(message, f"An error occurred: {str(e)}")
 
+
+# Function for searching and removing swear words from users in the monitoring list
 @bot.message_handler(func=lambda message: True)
 async def checking_messages(message):
     text = message.text
@@ -173,27 +194,37 @@ async def checking_messages(message):
     text_cleaning = re.split(r'[,.\n? ]+', text)
     user_name = message.from_user.username
 
+    # Handling errors and searching/removing swear words from users from the monitoring list
     try:
+        # Reading files with a list of users and swear words
         with open("swearing.txt", "r") as swear_content:
             curses = swear_content.read().split()
-
         with open("users.txt", "r") as user_content:
             users_check = user_content.read().split()
 
-        print(f"Message checking: ID = {user_id}, Username = @{user_name}")
+        print(f"Message checking: ID = {user_id}, Username = @{user_name}")  # Debugging to the console
 
+        # Searching for a user in the list of users to monitoring
         if user_id in users_check:
+            # Searching for swear words in user's text
             for word in text_cleaning:
                 for swear in curses:
+                    # Comparison of a user's word from the text with a swear word from the list
                     similarity = SequenceMatcher(None, word.lower(), swear.lower()).ratio()
+                    # If the match is greater than or equal to 60%, then we delete the user’s message
                     if similarity >= 0.6:
-                        print(f"Message removed, swearing '{swear}' word '{word}' similarity {similarity}")
+                        print(f"Message removed, swearing '{swear}' word '{word}' similarity {similarity}")   # Debugging to the console
                         await bot.delete_message(message.chat.id, message.id)
                         return
+    # Sending an error message that files were not found
     except FileNotFoundError as e:
         print(f"File not found: {str(e)}")
+    # Sending an error message
     except Exception as e:
         print(f"An error occurred while checking messages: {str(e)}")
 
+
+# Running a bot
 if __name__ == '__main__':
+    print("KGB bot is turned on!")
     asyncio.run(bot.polling())
